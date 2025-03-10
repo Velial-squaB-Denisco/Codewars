d3d0d79103224f74dc310d4c502fa7922039717d
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-10 19:56:12 +0300

    v5.2

--- Python/Kata 6kyu Nesting_Structure_Comparison.py
+++ Python/Kata 6kyu Nesting_Structure_Comparison.py
@@ -1,4 +1,5 @@
-"""Дополните функцию/метод (в зависимости от языка), чтобы они возвращали true/True, если их аргументом является массив с той же структурой вложенности и той же длиной вложенных массивов, что и первый массив.
+"""Дополните функцию/метод (в зависимости от языка), чтобы они возвращали true/True,
+если их аргументом является массив с той же структурой вложенности и той же длиной вложенных массивов, что и первый массив.
 
 Например:
 
@@ -16,5 +17,20 @@ same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
 # should return False
 same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )"""
 
-def same_structure_as(original,other):
-    pass
+def same_structure_as(original, other):
+
+    if isinstance(original, list) != isinstance(other, list):
+        return False
+
+    if not isinstance(original, list):
+        return True
+
+    if len(original) != len(other):
+        return False
+    
+    for o, e in zip(original, other):
+        if not same_structure_as(o, e):
+            return False
+    return True
+
+print(same_structure_as([1,[1,1]],[2,[2,2]]))

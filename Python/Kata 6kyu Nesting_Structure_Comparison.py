c1a4bfb85321a1a1066e3e95b303709f005263ba
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-10 19:26:21 +0300

    v5

---
+++ Python/Kata 6kyu Nesting_Structure_Comparison.py
@@ -0,0 +1,20 @@
+"""Дополните функцию/метод (в зависимости от языка), чтобы они возвращали true/True, если их аргументом является массив с той же структурой вложенности и той же длиной вложенных массивов, что и первый массив.
+
+Например:
+
+# should return True
+same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+
+# should return False 
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
+
+# should return True
+same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
+
+# should return False
+same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )"""
+
+def same_structure_as(original,other):
+    pass

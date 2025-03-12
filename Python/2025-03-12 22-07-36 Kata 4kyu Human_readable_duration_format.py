f76baa4563f95906a33092b34b06431ba5034f6f
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-12 22:10:39 +0300

    v7.1

---
+++ Python/2025-03-12 22-07-36 Kata 4kyu Human_readable_duration_format.py
@@ -0,0 +1,15 @@
+"""Ваша задача — написать функцию, которая форматирует продолжительность, заданную в секундах, 
+в удобном для восприятия человеком виде.
+
+Функция должна принимать неотрицательное целое число. Если оно равно нулю, функция просто возвращает "now". 
+В противном случае продолжительность выражается комбинацией years, days, hours, minutes и seconds.
+
+Это гораздо легче понять на примере:
+
+* For seconds = 62, your function should return 
+    "1 minute and 2 seconds"
+* For seconds = 3662, your function should return
+    "1 hour, 1 minute and 2 seconds"""
+
+def format_duration(seconds):
+    pass

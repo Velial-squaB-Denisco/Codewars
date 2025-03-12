328aa6a16d6778008d3a00832041ef0693fdb460
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-12 17:55:04 +0300

    v6.1

--- Python/Create file.py
+++ Python/Create file.py
@@ -1,12 +1,15 @@
-def create_file(n, name):
+from datetime import date
 
-    filename = f"Kata {n}kyu {name.replace(' ', '_')}.py"
+def create_file(n, name, current_time):
+
+    filename = f"{current_time} Kata {n}kyu {name.replace(' ', '_')}.py"
 
     with open(filename, 'w', encoding='utf-8') as file:
         file.write() 
 
 def main():
-    create_file(6, "Count IP Addresses")
+    current_time = date.today()
+    create_file(6, "Count IP Addresses", current_time)
 
 if __name__ == "__main__":
     main()

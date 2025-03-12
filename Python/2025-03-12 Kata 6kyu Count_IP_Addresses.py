800d62533e0d5a80f330241aee08982d98689bff
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-12 18:19:12 +0300

    v6.2

---
+++ Python/2025-03-12 Kata 6kyu Count_IP_Addresses.py
@@ -0,0 +1,23 @@
+def ips_between(start ,end):
+    
+    i = 0
+    j = 0
+
+    startlist = str(start).split()
+    endlist = str(end).split()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0
+
+print(ips_between("256.256.0.0", "256.256.1.0"))

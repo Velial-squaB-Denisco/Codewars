7848071f047507d65083fb506c3c7d9c1bde07f9
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-13 19:23:01 +0300

    v8

--- Python/Create file.py
+++ Python/Create file.py
@@ -2,15 +2,15 @@ import datetime
 
 def create_file(n, name, current_time):
 
-    filename = f"{current_time} Kata {n}kyu {name.replace(' ', '_')}.py"
+    filename = f"{current_time} Kata {n}kyu {str(name).replace(' ', '_')}.py"
 
     with open(filename, 'w', encoding='utf-8') as file:
-        file.write() 
+        file.write()
 
 def main():
     current_time = datetime.datetime.today()
     current_time = current_time.strftime("%Y-%m-%d %H-%M-%S")
-    create_file(4, "Human readable duration format", current_time)
+    create_file(5, "Can you get the loop", current_time)
 
 if __name__ == "__main__":
     main()

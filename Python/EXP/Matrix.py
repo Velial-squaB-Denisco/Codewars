d87af25d07b243b61bf219d97243fe2c4328abdd
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-14 17:33:20 +0300

    v9.3.3

--- Python/EXP/Matrix.py
+++ Python/EXP/Matrix.py
@@ -15,3 +15,13 @@ def create_matrix(N, value):
     return np.full((N, N), value)
 
 print(create_matrix(2, 4))
+
+# Заполнение матрицы II
+
+import numpy as np
+
+def create_matrix(N, value):
+    matrix = np.full((N, N), range(value))  
+    return matrix
+
+print(create_matrix(2, 4))

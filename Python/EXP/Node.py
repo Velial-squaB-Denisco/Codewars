69d4175eeab744dff3f79157ffa88c0f44fd0a36
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-14 15:39:49 +0300

    v8.3

--- Python/EXP/Node.py
+++ Python/EXP/Node.py
@@ -5,3 +5,16 @@ class Node:
 
     def __str__(self):
         return f"[{self.data}] -> {self.next}"
+    
+def loop_size(node):
+
+    nodes = []
+
+    for i in range(node):
+        nodes.append(Node(i))
+        if i > 0:
+            nodes[i-1].next = nodes[i]
+
+    nodes[-1].next = nodes[0]
+
+    return len(nodes)

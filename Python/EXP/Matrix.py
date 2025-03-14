5710c17a02866b041e62e7f1fd788c1c1e835d21
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-14 17:58:56 +0300

    v9.4.2

--- Python/EXP/Matrix.py
+++ Python/EXP/Matrix.py
@@ -38,3 +38,25 @@ def create_matrix(N, value):
     return matrix
 
 print("Заполнение матрицы III","\n", create_matrix(2, 4))
+
+# Заполнение матрицы IV
+
+import random
+import numpy as np
+
+def create_matrix(N):
+
+    # Генерация случайного целого числа в диапазоне от 1 до 10
+    value = random.randint(1, 10)
+    matrix = np.empty((N, N), dtype=int) # Стандартно dtupe = float
+    for i in range(N):
+        for j in range(N):
+            matrix[i][j] = int(value)
+
+    return matrix
+
+print("Заполнение матрицы IV","\n", create_matrix(2))
+
+# Генерация случайного числа с плавающей запятой в диапазоне от 0 до 1
+random_float = random.random()
+print(random_float)

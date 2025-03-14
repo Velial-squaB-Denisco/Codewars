7b76c399a1b3320fa0c1634d2fd2efb338602e24
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-14 16:19:02 +0300

    v8.8

--- Python/Create file.py
+++ Python/Create file.py
@@ -2,14 +2,14 @@ import datetime
 
 def create_file(n, name, current_time):
 
-    filename = f"{current_time} Kata {n}kyu {str(name).replace(' ', '_')}.py"
+    filename = f"{current_time} Kata {n}kyu {str(name)}.py"
 
     with open(filename, 'w', encoding='utf-8') as file:
         file.write()
 
 def main():
     current_time = datetime.datetime.today()
-    current_time = current_time.strftime("%Y-%m-%d %H-%M-%S")
+    current_time = current_time.strftime("%d-%m-%Y %H-%M-%S") #%Y-%m-%d
     create_file(4, "Matrix Determinant", current_time)
 
 if __name__ == "__main__":

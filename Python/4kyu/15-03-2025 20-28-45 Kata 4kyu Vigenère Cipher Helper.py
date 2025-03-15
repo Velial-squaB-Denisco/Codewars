86f491e1e10d13687bb53bae8d796c3d18427ac4
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-16 01:00:05 +0300

    e4

--- Python/4kyu/15-03-2025 20-28-45 Kata 4kyu Vigenère Cipher Helper.py
+++ Python/4kyu/15-03-2025 20-28-45 Kata 4kyu Vigenère Cipher Helper.py
@@ -59,6 +59,7 @@ class VigenereCipher(object):
         self.alphabet = alphabet
     
     def encode(self, text):
+        
         try:
             self.text = text
 
@@ -99,7 +100,45 @@ class VigenereCipher(object):
             return self.text
     
     def decode(self, text):
-        pass
+
+        try:
+            self.text = text
+
+            keyi = 0
+            n = len(self.alphabet)
+            newkey = list()
+
+            for a in self.text:
+                newkey.append(self.key[keyi])
+                keyi = (keyi + 1) % len(self.key)
+
+            res1 = []
+            for i in range(len(self.text)):
+                for j in range(len(self.alphabet)):
+                    if self.text[i] == self.alphabet[j]:
+                        res1.append(j)
+
+            res2 = []
+            for i in range(len(newkey)):
+                for j in range(len(self.alphabet)):
+                    if newkey[i] == self.alphabet[j]:
+                        res2.append(j)
+
+            mod = []
+            for i in range(len(self.text)):
+                mod.append((res1[i] + res2[i]) % n)
+
+            res = []
+            for i in mod:
+                res.append(self.alphabet[i])
+
+            res = ''.join(res)
+
+            return res
+        
+        except:
+
+            return self.text
 
 conf = VigenereCipher("key", 'abcdefghijklmnopqrstuvwxyz')
 e = conf.encode("CODEWARS")

06fac875342784b8cc9fb3ecae54eaeb1d0eb85e
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-16 21:15:12 +0300

    v11.11

--- Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
+++ Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
@@ -16,24 +16,30 @@ next_smaller(1027) == -1  # 0721 is out since we don't write numbers with leadin
 некоторые тесты будут включать очень большие числа.
 в тестовых данных используются только положительные целые числа."""
 
-def next_smaller(n):
-    m = n
-    nm2 = []
+# def next_smaller(n):
+#     m = n
+#     nm2 = []
 
-    n = str(n)
-    n = list(n)
-    for i in range(len(n)):
-        nm = list()
-        for j in range(len(n)):
-            nm.append(n[j - i])
-            nm1 = ''.join(nm)
-            if m > int(nm1):
-                nm2.append(int(nm1))
-                nm2.sort()
-                print("Y ", int(m), int(nm1), nm2)
-    if len(nm2) > 0:
-        return nm2[-1]
-    else:    
-        return -1
+#     n = str(n)
+#     n = list(n)
+#     for i in range(len(n)):
+#         nm = list()
+#         for j in range(len(n)):
+#             nm.append(n[j - i])
+#             nm1 = ''.join(nm)
+#             if m > int(nm1):
+#                 nm2.append(int(nm1))
+#                 nm2.sort()
+#                 print("Y ", int(m), int(nm1), nm2)
+#     if len(nm2) > 0:
+#         return nm2[-1]
+#     else:    
+#         return -1
+
+# print(next_smaller(907))
+
+def next_smaller(n):
+    m = list(str(n))
+    print(m)
 
 print(next_smaller(907))

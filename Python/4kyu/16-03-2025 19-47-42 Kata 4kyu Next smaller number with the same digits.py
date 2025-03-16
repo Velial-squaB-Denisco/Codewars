cecd5fd41311a3a73a76c8948c2c75b7600dde56
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-16 21:06:33 +0300

    v11.9

--- Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
+++ Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
@@ -18,6 +18,7 @@ next_smaller(1027) == -1  # 0721 is out since we don't write numbers with leadin
 
 def next_smaller(n):
     m = n
+    nm2 = []
 
     n = str(n)
     n = list(n)
@@ -27,8 +28,10 @@ def next_smaller(n):
             nm.append(n[j - i])
             nm1 = ''.join(nm)
             if m > int(nm1):
-                print("Y ", int(nm1), int(m))
+                nm2.append(int(nm1))
+                nm2.sort()
+                print("Y ", int(m), int(nm1), nm2)
             
-    return 0
+    return nm2[-1]
 
 print(next_smaller(907))

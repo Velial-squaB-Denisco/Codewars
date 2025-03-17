de0f30e9b54cdb070207950e12f45830bcda49d3
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-17 19:48:40 +0300

    v11.19

--- Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
+++ Python/4kyu/16-03-2025 19-47-42 Kata 4kyu Next smaller number with the same digits.py
@@ -39,4 +39,35 @@ def next_smaller(n):
         except:
             return -1
 
-print(next_smaller(135))
+print(next_smaller(100))
+
+#////////////////////////////////////////////////
+
+def next_smaller(n):
+    digits = list(str(n))
+    i = len(digits) - 2
+    # Ищем точку i, где digits[i] > digits[i+1]
+    while i >= 0 and digits[i] <= digits[i+1]:
+        i -= 1
+    if i == -1:
+        return -1  # Невозможно сделать меньше
+    
+    # Ищем j, наибольший индекс справа от i, где digits[j] < digits[i]
+    j = len(digits) - 1
+    while digits[j] >= digits[i]:
+        j -= 1
+    
+    # Меняем местами i и j
+    digits[i], digits[j] = digits[j], digits[i]
+    
+    # Переворачиваем оставшуюся часть после i
+    digits[i+1:] = reversed(digits[i+1:])
+    
+    # Проверяем на ведущий ноль
+    if digits[0] == '0':
+        return -1
+    
+    # Преобразуем обратно в число
+    next_num = int(''.join(digits))
+    
+    return next_num if next_num < n else -1

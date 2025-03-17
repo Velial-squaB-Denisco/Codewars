73df02d13441c47947ef14777296dd4e6b5cc0b3
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-17 20:48:43 +0300

    v13.1

--- Python/Create file.py
+++ Python/Create file.py
@@ -1,17 +1,16 @@
 import datetime
 
-def create_file(n, name, current_time):
-
-    filename = f"{current_time} Kata {n}kyu {str(name)}.py"
+def create_file(n, name, current_date):
+    filename = f"{current_date} Kata {n}kyu {str(name)}.py"
+    print(filename)
     path = f"{n}kyu"
 
-    with open(f"C:\\Users\\user\\Documents\\GitHub\Codewars\\Python\\{path}\\{filename}", 'w', encoding='utf-8') as file:
+    with open(f"C:\\Users\\user\\Documents\\GitHub\\Codewars\\Python\\{path}\\{filename}", 'w', encoding='utf-8') as file:
         file.write('""""""')
 
 def main():
-    current_time = datetime.datetime.today()
-    current_time = current_time.strftime("%d-%m-%Y %H-%M-%S") #%Y-%m-%d
-    create_file(3, "Rail Fence Cipher: Encoding and Decoding", current_time)
+    current_date = datetime.datetime.today().strftime("%d-%m-%Y")
+    create_file(3, "Rail Fence Cipher Encoding and Decoding", current_date)
 
 if __name__ == "__main__":
     main()

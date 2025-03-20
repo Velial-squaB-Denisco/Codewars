3d9c4c0a1c14e8157cec51b38f933e85dc3ce5c6
Velial-squaB-Denisco <ppoi42477@gmail.com>
2025-03-20 15:21:07 +0300

    v5.14

--- Python/3kyu/17-03-2025 Kata 3kyu Rail Fence Cipher Encoding and Decoding.py
+++ Python/3kyu/17-03-2025 Kata 3kyu Rail Fence Cipher Encoding and Decoding.py
@@ -58,9 +58,41 @@ def encode_rail_fence_cipher(string, n):
 def decode_rail_fence_cipher(string, n):
 
     #//////
+    matrix = np.full((n, len(string)), '', dtype=str)
+    j = 0
+    d = 1
 
-    for i in range(0, len(string), n):
-        print(string[i:i + n])
+    for i in range(len(string)):
+        matrix[j][i] = '*'
+
+        if j == n - 1:
+            d = -1
+        elif j == 0:
+            d = 1
+
+        j += d
+
+    index = 0
+    for i in range(n):
+        for j in range(len(string)):
+            if matrix[i][j] == '*':
+                matrix[i][j] = string[index]
+                index += 1
+
+    res = ""
+    j = 0
+    d = 1
+    for i in range(len(string)):
+        res += matrix[j][i]
+
+        if j == n - 1:
+            d = -1
+        elif j == 0:
+            d = 1
+
+        j += d
+
+    return res
     
     #//////
 
